577edc5721bd7bc50b409d67eae45917c418dde5
panupong <tayalone@gmail.com>
2018-04-22 18:39:57 +0700

    create upload single and multiple files

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const multer = require('multer');
+const multer = require('multer')
+const fs = require('fs.promised');
+
 
 //---- setting middleware
 const app = express()
@@ -14,10 +16,56 @@ app.use(bodyParser.urlencoded({
 }))
 // ------------------------
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        //console.log(file)
+      cb(null, './tmp')
+    },
+    filename: function (req, file, cb) {
+        //console.log(file)
+      cb(null,  Date.now()+"-"+file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+const upload2 = multer({ storage: storage })
+
+// --------- Router --------------------------------------
 app.get("/",(req, res) => {
     res.send("/")
 })
+app.post("/upload2",upload.single('file'),(req,res) => {
+    //res.send(req.file)
+    fs.unlink(req.file.path)
+        .then(() => {
+            console.log('file deleted successfully');
+            res.send(req.file)
+        })
+        .catch((err) => {
+            console.log('file deleted failed')
+            res.send(err)
+        })  
+})
+app.post("/uploadMulti",upload.array('photos',5),(req,res) => {
+    console.log(req.body)
+    const promisedOBJ = req.files.map(file => {
+        return fs.unlink(file.path)
+            .then(() => {
+                return true
+            })
+            .catch((err) => {
+                return false
+            })
+    })
+    Promise.all(promisedOBJ)
+    .then((values) => {
+        res.send(JSON.stringify(req.files))
+      })
+    .catch((err) => {
+        res.send(err)
+    })
+})
 
+//--------------------------------------------------------
 
 const server = app.listen(8888, () =>{
     console.log("This App running at 8888")
